2ae5da8258b8beabca86b444d1cdf81cf75de181
Chrislee156106 <103225544+Chrislee156106@users.noreply.github.com>
2024-07-13 18:37:58 +0000

    turning in late work please confirm it

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const excuse = [
+  "A lonely jogger kicked my sandwich on the street.",
+  "My dog ate my homework.",
+  "I had a flat tire.",
+  "I missed the bus.",
+  "My alarm didn't go off.",
+  "I was feeling under the weather.",
+  "I had a family emergency.",
+  "I got stuck in traffic.",
+  "I lost track of time.",
+  "I had to take care of a sick friend.",
+  "My computer crashed."
+];
+function generateRandomExcuse() {
+  return excuse[Math.floor(Math.random() * excuse.length)];
+}
+document.querySelector(".secondary-header").innerText = generateRandomExcuse();
